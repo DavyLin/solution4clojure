fb4f764ebc270bdeb8d0a269daaa9de59ccb26e2
DavyLin <linjxljx@gmail.com>
2013-07-03 09:30:39 +0800

    add other solutions for problem

---
+++ solutions/12.Intro to Sequences.clj
@@ -0,0 +1,5 @@
+﻿(= __ (first '(3 2 1)))
+(= __ (second [2 3 4]))
+(= __ (last (list 1 2 3)))
+
+;solution: 3
---
+++ solutions/6.Intro to Vectors.clj
@@ -0,0 +1,3 @@
+﻿(= [__] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+;solution:   :a :b :c
