0cbf1ce3252f2797dc383598b776f24dba6bc476
DavyLin <linjxljx@gmail.com>
2013-07-09 21:00:28 +0800

    add one solution

---
+++ solutions/173.Intro to Destructring 2.clj
@@ -0,0 +1,7 @@
+﻿;Sequential destructuring allows you to bind symbols to parts of sequential things (vectors, lists, seqs, etc.): (let [bindings* ] exprs*) Complete the bindings so all let-parts evaluate to 3.
+(= 3
+  (let [[__] [+ (range 3)]] (apply __))
+  (let [[[__] b] [[+ 1] 2]] (__ b))
+  (let [[__] [inc 2]] (__)))
+  
+;solution: %2 %1
