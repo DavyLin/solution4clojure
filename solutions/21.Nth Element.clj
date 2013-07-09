ba1a2f8c0704f12193ac919080d94edf8b016c9f
DavyLin <linjxljx@gmail.com>
2013-07-09 19:09:23 +0800

    add more solutions

---
+++ solutions/21.Nth Element.clj
@@ -0,0 +1,7 @@
+﻿;Write a function which returns the Nth element from a sequence.
+(= (__ '(4 5 6 7) 2) 6)
+(= (__ [:a :b :c] 0) :a)
+(= (__ [1 2 3 4] 1) 2)
+(= (__ '([1 2] [3 4] [5 6]) 2) [5 6])
+
+;solution: #(first (drop %2 %1))
