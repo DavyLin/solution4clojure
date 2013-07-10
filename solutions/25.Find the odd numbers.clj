a14c1fb81211e3bc3c8384326a18de5f18a5031a
DavyLin <linjxljx@gmail.com>
2013-07-10 20:36:57 +0800

    add more solution

---
+++ solutions/25.Find the odd numbers.clj
@@ -0,0 +1,17 @@
+﻿;Write a function which returns only the odd numbers from a sequence.
+	
+
+(= (__ #{1 2 3 4 5}) '(1 3 5))
+	
+
+(= (__ [4 2 1 6]) '(1))
+	
+
+(= (__ [2 2 4 6]) '())
+	
+
+(= (__ [1 1 1 3]) '(1 1 1 3))
+
+;solution:
+
+#(filter odd? %)
