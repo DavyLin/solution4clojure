3290259ce3490490fd010ed9422d6b15ebe635e5
DavyLin <linjxljx@gmail.com>
2013-07-10 09:51:58 +0800

    add two solutions

---
+++ solutions/22.Count a Sequence.clj
@@ -0,0 +1,25 @@
+﻿;Write a function which returns the total number of elements in a sequence.
+	
+
+(= (__ '(1 2 3 3 1)) 5)
+	
+
+(= (__ "Hello World") 11)
+	
+
+(= (__ [[1 2] [3 4] [5 6]]) 3)
+	
+
+(= (__ '(13)) 1)
+	
+
+(= (__ '(:a :b :c)) 3)
+
+
+;solution:
+;; see qiuxiafei's answer
+#(loop [result 0 c %]
+    (if(empty? c) result
+    (recur (inc result) (rest c))))
+;; others' excellent anser
+#(reduce (fn [x y] (inc x)) 0 %)
---
+++ solutions/24.Sum It All Up.clj
@@ -0,0 +1,14 @@
+﻿;Sum It All up
+
+;Write a function which returns the sum of a sequence of numbers.
+(= (__ [1 2 3]) 6)
+
+(= (__ (list 0 -2 5 5)) 8)
+
+(= (__ #{4 2 1}) 7)
+
+(= (__ '(0 0 -1)) -1)
+
+(= (__ '(1 10 3)) 14)
+
+;solution: (fn [coll] (apply + coll))
