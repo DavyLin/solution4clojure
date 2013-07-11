417813eb3b13d41589f58ef9b8834776291caf9b
DavyLin <linjxljx@gmail.com>
2013-07-11 21:08:17 +0800

    add more solutions

---
+++ solutions/26.Fibonacci Sequence.clj
@@ -0,0 +1,16 @@
+﻿;Write a function which returns the first X fibonacci numbers.
+	
+
+(= (__ 3) '(1 1 2))
+	
+
+(= (__ 6) '(1 1 2 3 5 8))
+	
+
+(= (__ 8) '(1 1 2 3 5 8 13 21))
+
+;solution: 
+
+#(take % (map last(iterate (fn [[x y]] [y (+ x y)]) [0 1])))
+
+;see the solution at qiuxiafei/4clojure
