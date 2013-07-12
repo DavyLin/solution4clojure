6fa721f240c19336531b792b23d96bc5d0821c79
DavyLin <linjxljx@gmail.com>
2013-07-12 17:19:31 +0800

    add one solution

---
+++ solutions/27.Palindrome Detector.clj
@@ -0,0 +1,22 @@
+﻿;Write a function which returns true if the given sequence is a palindrome.
+
+;Hint: "racecar" does not equal '(\r \a \c \e \c \a \r)
+	
+
+(false? (__ '(1 2 3 4 5)))
+	
+
+(true? (__ "racecar"))
+	
+
+(true? (__ [:foo :bar :foo]))
+	
+
+(true? (__ '(1 1 3 3 1 1)))
+	
+
+(false? (__ '(:a :b :c)))
+
+;solution:
+
+ #(= (seq %) (reverse %))
