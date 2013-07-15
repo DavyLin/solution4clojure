dd625140dbce0ebf83c1cb13311f24535eba8e89
DavyLin <linjxljx@gmail.com>
2013-07-15 23:04:03 +0800

    add one solution

---
+++ solutions/29.Get the Caps.clj
@@ -0,0 +1,18 @@
+﻿;Get the Caps
+ 
+;Difficulty:	Easy
+;Topics:	strings
+
+
+;Write a function which takes a string and returns a new string containing only the capital letters.
+	
+
+(= (__ "HeLlO, WoRlD!") "HLOWRD")
+	
+
+(empty? (__ "nothing"))
+	
+
+(= (__ "$#A(*&987Zf") "AZ")
+
+;solution : #(apply str (re-seq #"[A-Z]" %))
