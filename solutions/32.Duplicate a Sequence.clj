e2cab8e705cfcfbbfb31b642113e8c2b8d261965
DavyLin <linjxljx@gmail.com>
2013-07-15 08:49:19 +0800

    add more solution

---
+++ solutions/32.Duplicate a Sequence.clj
@@ -0,0 +1,16 @@
+﻿;Write a function which duplicates each element of a sequence.
+	
+
+(= (__ [1 2 3]) '(1 1 2 2 3 3))
+	
+
+(= (__ [:a :a :b :b]) '(:a :a :a :a :b :b :b :b))
+	
+
+(= (__ [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4]))
+	
+
+(= (__ [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4]))
+
+;solution:
+ #(interleave % %)
