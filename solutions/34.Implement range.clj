f873934b4f2c4ee9398fc96fd699539a755c8825
DavyLin <linjxljx@gmail.com>
2013-07-19 16:30:50 +0800

    add one solution

---
+++ solutions/34.Implement range.clj
@@ -0,0 +1,12 @@
+﻿;Write a function which creates a list of all integers in a given range.
+	
+
+(= (__ 1 4) '(1 2 3))
+	
+
+(= (__ -2 2) '(-2 -1 0 1))
+	
+
+(= (__ 5 8) '(5 6 7))
+
+;solution: #(take (- %2 %1) (iterate inc %1))
