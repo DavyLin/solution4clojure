38948a5a066c10f5035d1a58b4618f0aa95fdb4f
DavyLin <linjxljx@gmail.com>
2013-07-24 10:32:34 +0800

    update the solution

--- solutions/38.Maximum value.clj
+++ solutions/38.Maximum value.clj
@@ -16,3 +16,17 @@
 (fn max2([x] x)
    ([x y] (if (> x y) x y))  
    ([x y & more] (reduce max2 (max2 x y) more)))
+
+
+;other solution:
+
+ ;_pcl's solution:
+(fn [x & xs] (reduce #(if (> %2 %1) %2 %1) x xs))
+
+;adereth's solution:
+(fn [ & xs ]
+  (reduce #(if (> %1 %2) %1 %2) xs))
+
+;austintaylor's solution:
+(fn [& xs]
+  (reduce #(if (> %2 %1) %2 %1) xs))
