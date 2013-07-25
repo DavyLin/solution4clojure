67234690aab562edc426373c42ec0ae45d0d20b4
DavyLin <linjxljx@gmail.com>
2013-07-25 10:23:05 +0800

    add one solution

---
+++ solutions/30.Compress a Sequence.clj
@@ -0,0 +1,16 @@
+﻿;Compress a Sequence
+ 
+;Difficulty:	Easy
+;Topics:	seqs
+
+
+;Write a function which removes consecutive duplicates from a sequence.
+	
+
+(= (apply str (__ "Leeeeeerrroyyy")) "Leroy")
+	
+
+(= (__ [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
+	
+
+(= (__ [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
