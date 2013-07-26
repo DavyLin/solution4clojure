711bd70e5312784bb5d7af5eb3198c547ca7729c
DavyLin <linjxljx@gmail.com>
2013-07-26 16:50:20 +0800

    add one solution

---
+++ solutions/40.Interpose a Seq.clj
@@ -0,0 +1,18 @@
+﻿;Interpose a Seq
+ 
+;Difficulty:	Easy
+;Topics:	seqs core-functions
+
+
+;Write a function which separates the items of a sequence by an arbitrary value.
+	
+
+(= (__ 0 [1 2 3]) [1 0 2 0 3])
+	
+
+(= (apply str (__ ", " ["one" "two" "three"])) "one, two, three")
+	
+
+(= (__ :z [:a :b :c :d]) [:a :z :b :z :c :z :d])
+
+;the solution: #(drop 1 (interleave (repeat %1) %2))
