46375069b2739533ea0399f53e71de3d3a6a9245
DavyLin <linjxljx@gmail.com>
2013-07-26 10:32:08 +0800

    add one solution

---
+++ solutions/52.Intro to Destructuring.clj
@@ -0,0 +1,11 @@
+﻿;Intro to Destructuring
+ 
+;Difficulty:	Easy
+;Topics:	destructuring
+
+
+;Let bindings and function parameter lists support destructuring.
+	
+
+(= [2 4] (let [[a b c d e f g] (range)] __))
+;the solution: [c e]
