ca2eddc8da2e181e34dbbddf2b4385693f8bf7f6
DavyLin <linjxljx@gmail.com>
2013-07-27 17:10:35 +0800

    add one solution

---
+++ solutions/28.Flatten a Sequence.clj
@@ -0,0 +1,12 @@
+﻿;Write a function which flattens a sequence.
+	
+
+(= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+	
+
+(= (__ ["a" ["b"] "c"]) '("a" "b" "c"))
+	
+
+(= (__ '((((:a))))) '(:a))
+
+;the solution:#(filter (complement sequential?) (tree-seq sequential? identity %))
