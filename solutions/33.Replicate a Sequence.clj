5f7d5130499410532a628af6d8280295f14fbc27
DavyLin <linjxljx@gmail.com>
2013-07-28 21:18:56 +0800

    add one solution

---
+++ solutions/33.Replicate a Sequence.clj
@@ -0,0 +1,26 @@
+﻿Replicate a Sequence
+ 
+;Difficulty:	Easy
+;Topics:	seqs
+
+
+;Write a function which replicates each element of a sequence a variable number of times.
+	
+
+(= (__ [1 2 3] 2) '(1 1 2 2 3 3))
+	
+
+(= (__ [:a :b] 4) '(:a :a :a :a :b :b :b :b))
+	
+
+(= (__ [4 5 6] 1) '(4 5 6))
+	
+
+(= (__ [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
+	
+
+(= (__ [44 33] 2) [44 44 33 33])
+
+;solution: 
+#(apply concat (map (fn [input](repeat %2 input)) %1))
+;#(mapcat (fn [input](repeat %2 input)) %1)
