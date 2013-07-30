00b5690a62c941b67be8f37777fe81c0c0b6e2f2
DavyLin <linjxljx@gmail.com>
2013-07-30 09:22:06 +0800

    add one solution

---
+++ solutions/49.Split a sequence.clj
@@ -0,0 +1,21 @@
+﻿;Split a sequence
+ 
+;Difficulty:	Easy
+;Topics:	seqs core-functions
+
+;Write a function which will split a sequence into two parts.
+	
+
+(= (__ 3 [1 2 3 4 5 6]) [[1 2 3] [4 5 6]])
+	
+
+(= (__ 1 [:a :b :c :d]) [[:a] [:b :c :d]])
+	
+
+(= (__ 2 [[1 2] [3 4] [5 6]]) [[[1 2] [3 4]] [[5 6]]])
+
+
+;the solution: 
+#(vector 
+       (vec (take %1 %2)) 
+       (vec (drop %1 %2)))
