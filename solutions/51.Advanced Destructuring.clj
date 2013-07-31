59502b439b8a456b2e96d15ea0f951cac882292b
DavyLin <linjxljx@gmail.com>
2013-07-31 09:30:52 +0800

    add one solution

---
+++ solutions/51.Advanced Destructuring.clj
@@ -0,0 +1,12 @@
+﻿;Advanced Destructuring
+ 
+;Difficulty:	Easy
+;Topics:	destructuring
+
+
+;Here is an example of some more sophisticated destructuring.
+;test not run	
+
+(= [1 2 [3 4 5] [1 2 3 4 5]] (let [[a b & c :as d] __] [a b c d]))
+
+;the solution: [1 2 3 4 5]
