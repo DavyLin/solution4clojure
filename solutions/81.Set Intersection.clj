cbbad38dd5b0a1e6ecbbe16fb446c15b21101470
DavyLin <linjxljx@gmail.com>
2013-08-04 14:11:33 +0800

    udpate the solution

--- solutions/81.Set Intersection.clj
+++ solutions/81.Set Intersection.clj
@@ -3,6 +3,7 @@
 ;Difficulty:	Easy
 ;Topics:	set-theory
 
+
 ;Write a function which returns the intersection of two sets. The intersection is the sub-set of items that each set has in common.
 ;test not run	
 
