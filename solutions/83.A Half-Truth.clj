9266f42c7ca9874b033e5710509aec3a0b690225
DavyLin <linjxljx@gmail.com>
2013-08-05 09:31:17 +0800

    add the solution

---
+++ solutions/83.A Half-Truth.clj
@@ -0,0 +1,28 @@
+﻿;Write a function which takes a variable number of booleans. Your function should return true if some of the parameters are true, but not all of the parameters are true. Otherwise your function should return false.
+;test not run	
+
+(= false (__ false false))
+test not run	
+
+(= true (__ true false))
+test not run	
+
+(= false (__ true))
+test not run	
+
+(= true (__ false true false))
+test not run	
+
+(= false (__ true true true))
+test not run	
+
+(= true (__ true true true false))
+
+
+
+;the solution: 
+
+#(true? 
+  (and
+    (some true? %&)
+    (some false? %&)))
