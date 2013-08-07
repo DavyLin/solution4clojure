e25c0618d795e9a2028ff88bc1ad7e0a300ea74d
DavyLin <linjxljx@gmail.com>
2013-08-07 09:56:36 +0800

    add one solution

---
+++ solutions/62.Re-implement Iterate.clj
@@ -0,0 +1,19 @@
+﻿;Re-implement Iterate
+ 
+;Difficulty:	Easy
+;Topics:	seqs core-functions
+
+
+;Given a side-effect free function f and an initial value x write a function which returns an infinite lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc.
+;test not run	
+
+(= (take 5 (__ #(* 2 %) 1)) [1 2 4 8 16])
+test not run	
+
+(= (take 100 (__ inc 0)) (take 100 (range)))
+test not run	
+
+(= (take 9 (__ #(inc (mod % 3)) 1)) (take 9 (cycle [1 2 3])))
+
+(fn it [f x]
+  (lazy-seq (cons x (it f (f x)))))
