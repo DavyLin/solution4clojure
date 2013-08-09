8778f2bfd99bc7c89e6069568754ff9d65c9ffc8
DavyLin <linjxljx@gmail.com>
2013-08-09 14:04:15 +0800

    add one solution

---
+++ solutions/66.Greatest Common Divisor.clj
@@ -0,0 +1,42 @@
+﻿;Greatest Common Divisor
+ 
+;Difficulty:	Easy
+;Topics:	
+
+
+;Given two integers, write a function which returns the greatest common divisor.
+;test not run	
+
+(= (__ 2 4) 2)
+test not run	
+
+(= (__ 10 5) 5)
+test not run	
+
+(= (__ 5 7) 1)
+test not run	
+
+(= (__ 1023 858) 33)
+
+
+
+
+;the solution :
+(fn [v1 v2]
+  (apply max
+         (filter #(and (zero? (mod v1 %))
+                       (zero? (mod v2 %)))
+                 (range 1 (max v1 v2)))))
+                 
+;the others solution:
+(fn [a b]
+  (let [get-divisor (fn [n] (into #{}
+                              (filter #(zero? (rem n %))
+                                      (range 1 (inc n)))
+                              ))
+        a-divisor (get-divisor a)
+        b-divisor (get-divisor b)
+        commom-divisor (clojure.set/intersection a-divisor b-divisor)]
+    (apply max commom-divisor)))
+
+(fn [x y] (apply max (filter #(= 0 (mod x %) (mod y %)) (range 1 (+ 1 (max (/ x 2) (/ y 2)))))))
